19d1b5cfccf5e5f7fe34aeb6d351e7ad0f46edf3
Maciej Sierzputowski <72218534+WalkmanOfWar@users.noreply.github.com>
2023-03-15 20:39:19 +0100

    Add spring-backend dir

---
+++ spring-backend/src/main/java/pl/technologie_handlu_elektronicznego/ksiegarnia/KsiegarniaApplication.java
@@ -0,0 +1,13 @@
+package pl.technologie_handlu_elektronicznego.ksiegarnia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsiegarniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsiegarniaApplication.class, args);
+	}
+
+}
---
+++ spring-backend/src/test/java/pl/technologie_handlu_elektronicznego/ksiegarnia/KsiegarniaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.technologie_handlu_elektronicznego.ksiegarnia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsiegarniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
