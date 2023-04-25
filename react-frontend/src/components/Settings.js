4f7fec65e6aad65f00f1e5f4cfc84e6bffe5e1cf
Alt-az <235979@edu.p.lodz.pl>
2023-04-25 22:40:57 +0200

    adding buttons

--- react-frontend/src/components/Settings.js
+++ react-frontend/src/components/Settings.js
@@ -2,7 +2,16 @@ import "./Settings.css"
 function Settings() {
     return (
       <div class="container settings-block">
-
+        <div class= "row">
+            <div class="col-4">
+                <div class="btn-group-vertical">
+                    <button type="button" class="btn btn-light">Profile</button>
+                    <button type="button" class="btn btn-light">Samsung</button>
+                    <button type="button" class="btn btn-light">Sony</button>
+                </div>
+            </div>
+            <div class="col-8">dsad</div>
+        </div>
       </div>
     );
   }
