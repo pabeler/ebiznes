71cfc3e9468be7eabf5b8fabf28ab1b21f10a34d
Alt-az <235979@edu.p.lodz.pl>
2023-04-26 12:20:02 +0200

    photo edit

--- react-frontend/src/components/Settings.js
+++ react-frontend/src/components/Settings.js
@@ -9,24 +9,40 @@ function Settings() {
                     <button type="button" class="btn btn-light btn-lg">Appearance</button>
                 </div>
           <div class="col-8 container settings-block">
-                <div class="col-8">
-                <form action="/action_page.php">
-                    <div class="mb-3 mt-3">
-                        <label for="email" class="form-label">Name:</label>
-                        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email"/>
+                
+                <div class="row">
+                    <div class="col-4 container">
+                        <form action="/action_page.php">
+                            <div class="mb-3 mt-3">
+                                <label for="email" class="form-label">Name:</label>
+                                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email"/>
+                            </div>
+                            <div class="mb-3 mt-3">
+                                <label for="email" class="form-label">Email:</label>
+                                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email"/>
+                            </div>
+                            <div class="mb-3">
+                                <label for="pwd" class="form-label">Password:</label>
+                                <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd"/>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Update Profile</button>
+                        </form>
                     </div>
-                    <div class="mb-3 mt-3">
-                        <label for="email" class="form-label">Email:</label>
-                        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email"/>
+                    <div class="col-4 container">
+                        <div class="dropdown">
+                            <div class="dropdown-toggle" data-bs-toggle="dropdown">
+                                <img class="img-thumbnail avatar-settings " src="images\avatar.png" alt="avatar"/>
+                            </div>
+                            
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Upload a photo</a></li>
+                                <li><a class="dropdown-item" href="#">Remove photo</a></li>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="mb-3">
-                        <label for="pwd" class="form-label">Password:</label>
-                        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd"/>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Update Profile</button>
-                </form>
                 </div>
             </div>
+            
       </div>
     );
   }
