84b669d296c3e7198322c81f10f1422ccbd39172
pabeler <pabeler@o2.pl>
2023-05-05 09:56:14 +0200

    Fix settings site

--- react-frontend/src/components/Settings.js
+++ react-frontend/src/components/Settings.js
@@ -1,8 +1,78 @@
 import "./Settings.css"
+import {Button, Col, Container, Row} from "react-bootstrap";
 function Settings() {
     return (
-        
-      <div class = "row">
+        <Container>
+            <Row >
+                <Col>
+                    <h2>Dane Konta</h2>
+                    <Row>
+                        <label htmlFor="email" className="form-label">Nazwa użytkownika:</label>
+                        <input type="name" className="form-control" id="name" placeholder="Wpisz nazwe użytkownika"
+                              name="name"/>
+                    </Row>
+                    <Row>
+                        <label htmlFor="email" className="form-label">Email:</label>
+                        <input type="email" className="form-control" id="email" placeholder="Wpisz email" name="email"/>
+                    </Row>
+                    <Row>
+                        <label htmlFor="pwd" className="form-label">Hasło:</label>
+                        <input type="password" className="form-control" id="pwd" placeholder="Wpisz hasło" name="pswd"/>
+                    </Row>
+                </Col>
+                <Col>
+                    <h2>Dane Adresowe</h2>
+                    <Row>
+                        <label htmlFor="email" className="form-label">Miasto:</label>
+                        <input type="name" className="form-control" id="name" placeholder="Wpisz nazwe Miasta"
+                               name="name"/>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <label htmlFor="email" className="form-label">Ulica:</label>
+                            <input type="email" className="form-control" id="email" placeholder="Wpisz ulice"
+                                   name="email"/>
+                        </Col>
+                        <Col xs={4}>
+                            <label htmlFor="email" className="form-label">Numer domu:</label>
+                            <input type="email" className="form-control" id="email" placeholder="Wpisz numer domu"
+                                   name="email"/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <label htmlFor="pwd" className="form-label">Kod pocztowy:</label>
+                        <input type="password" className="form-control" id="pwd" placeholder="Wpisz kod pocztowy"
+                               name="pswd"/>
+                    </Row>
+                </Col>
+                <Col>
+                    <h2>Dane użytkownika</h2>
+                    <Row>
+                        <label for="pwd" class="form-label">Imie i nazwisko:</label>
+                        <input type="email" class="form-control" id="email" placeholder="Wpisz imie i nazwisko" name="email"/>
+                    </Row>
+                    <Row>
+                        <label for="pwd" class="form-label">Data narodzin:</label>
+                        <input type="email" class="form-control" id="email" placeholder="Wpisz date urodzin" name="email"/>
+                    </Row>
+                    <Row>
+                        <label for="pwd" class="form-label">Numer Telefonu:</label>
+                        <input type="email" class="form-control" id="email" placeholder="Wpisz numer telefonu" name="email"/>
+                    </Row>
+                </Col>
+            </Row>
+            <Row>
+            </Row>
+            <Row>
+                <Col>
+                    <Button variant="primary" type="submit">
+                        Zapisz zmiany
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+
+      /*<div class = "row">
           <div class="col-8 container settings-block">
                 <div class="row">
                     <div class="col-4 container">
@@ -19,7 +89,7 @@ function Settings() {
                                 <label for="pwd" class="form-label">Hasło:</label>
                                 <input type="password" class="form-control" id="pwd" placeholder="Wpisz hasło" name="pswd"/>
                             </div>
-                            
+
                     </div>
                     <div class="col-4 container">
                         <h2>Dane Adresowe</h2>
@@ -36,7 +106,7 @@ function Settings() {
                                 <label for="email" class="form-label">Numer domu:</label>
                                 <input type="email" class="form-control" id="email" placeholder="Wpisz numer domu" name="email"/>
                             </div>
-                                
+
                             </div>
                             <div class="mb-3">
                                 <label for="pwd" class="form-label">Kod pocztowy:</label>
@@ -63,10 +133,10 @@ function Settings() {
                     </div>
                     </div>
                 </div>
-                
-                
-                
-      </div>
+
+
+
+      </div>*/
     );
   }
   
