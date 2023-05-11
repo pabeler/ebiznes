b63bd1f67d1be0856ec1c3717a60186e34dbc02b
pabeler <pabeler@o2.pl>
2023-05-11 21:07:20 +0200

    Add forms to Settings.js

--- react-frontend/src/components/Settings.js
+++ react-frontend/src/components/Settings.js
@@ -1,73 +1,210 @@
 import "./Settings.css"
-import {Link} from "react-router-dom";
+import {Button, Card, Form} from "react-bootstrap";
+import {ToastContainer} from "react-toastify";
+import {useState} from "react";
+
 function Settings() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [name, setName] = useState('');
+    const [secondName, setSecondName] = useState('');
+    const [birthDate, setBirthDate] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+
+    const [country, setCountry] = useState('');
+    const [city, setCity] = useState('');
+    const [street, setStreet] = useState('');
+    const [houseNumber, setHouseNumber] = useState('');
+    const [apartmentNumber, setApartmentNumber] = useState('');
+    const [postalCode, setPostalCode] = useState('');
+
+    const [showAccountDetails, setShowAccountDetails] = useState(true);
+    const [showAddressDetails, setShowAddressDetails] = useState(false);
+    const [showChangeCredentials, setShowChangeCredentials] = useState(false);
+
+    const handleChangeCredentials = (event) => {
+        event.preventDefault();
+        setEmail('');
+        setPassword('');
+    };
+
+    const handleAccountDetails = (event) => {
+        event.preventDefault();
+        setName('')
+        setSecondName('')
+        setBirthDate('')
+        setPhoneNumber('')
+    };
+
+    const handleAddressDetails = (event) => {
+        event.preventDefault();
+        setCountry('')
+        setCity('')
+        setStreet('')
+        setHouseNumber('')
+        setApartmentNumber('')
+        setPostalCode('')
+    }
+
+    const goToAccountDetails = (event) => {
+        event.preventDefault();
+        setShowAccountDetails(true);
+        setShowAddressDetails(false);
+        setShowChangeCredentials(false);
+    };
+
+    const goToAddressDetails = (event) => {
+        event.preventDefault();
+        setShowAccountDetails(false);
+        setShowAddressDetails(true);
+        setShowChangeCredentials(false);
+    };
+
+    const goToChangeCredentials = (event) => {
+        event.preventDefault();
+        setShowAccountDetails(false);
+        setShowAddressDetails(false);
+        setShowChangeCredentials(true);
+    }
+
     return (
-        
-      <div class = "row">
-          <div class="col-8 container settings-block">
-                <div class="row">
-                    <div class="col-4 container">
-                            <h2>Dane Konta</h2>
-                            <div class="mb-3 mt-3">
-                                <label for="email" class="form-label">Nazwa użytkownika:</label>
-                                <input type="name" class="form-control" id="name" placeholder="Wpisz nazwe użytkownika" name="name"/>
-                            </div>
-                            <div class="mb-3 mt-3">
-                                <label for="email" class="form-label">Email:</label>
-                                <input type="email" class="form-control" id="email" placeholder="Wpisz email" name="email"/>
-                            </div>
-                            <div class="mb-3">
-                                <label for="pwd" class="form-label">Hasło:</label>
-                                <input type="password" class="form-control" id="pwd" placeholder="Wpisz hasło" name="pswd"/>
-                            </div>
-                            
-                    </div>
-                    <div class="col-4 container">
-                        <h2>Dane Adresowe</h2>
-                            <div class="mb-3 mt-3">
-                                <label for="email" class="form-label">Miasto:</label>
-                                <input type="name" class="form-control" id="name" placeholder="Wpisz nazwe Miasta" name="name"/>
-                            </div>
-                            <div class="row mb-3 mt-3">
-                                <div class="col-8">
-                                <label for="email" class="form-label">Ulica:</label>
-                                <input type="email" class="form-control" id="email" placeholder="Wpisz ulice" name="email"/>
-                            </div>
-                                <div class="col-4">
-                                <label for="email" class="form-label">Numer domu:</label>
-                                <input type="email" class="form-control" id="email" placeholder="Wpisz numer domu" name="email"/>
-                            </div>
-                                
-                            </div>
-                            <div class="mb-3">
-                                <label for="pwd" class="form-label">Kod pocztowy:</label>
-                                <input type="password" class="form-control" id="pwd" placeholder="Wpisz kod pocztowy" name="pswd"/>
-                            </div>
-                    </div>
-                    <div>
-                        <h2>Dane użytkownika</h2>
-                        <div class="mb-3 container col-2">
-                            <label for="pwd" class="form-label">Imie i nazwisko:</label>
-                            <input type="email" class="form-control" id="email" placeholder="Wpisz imie i nazwisko" name="email"/>
-                        </div>
-                        <div class="mb-3 container col-2">
-                            <label for="pwd" class="form-label">Data narodzin:</label>
-                            <input type="email" class="form-control" id="email" placeholder="Wpisz date urodzin" name="email"/>
-                        </div>
-                        <div class="mb-3 container col-2">
-                            <label for="pwd" class="form-label">Numer Telefonu:</label>
-                            <input type="email" class="form-control" id="email" placeholder="Wpisz numer telefonu" name="email"/>
-                        </div>
-                    </div>
-                    <div class="container col-2">
-                        <button type="submit" class="btn btn-primary">Zapisz zmiany</button>
-                    </div>
+        <>
+            <div className="container mt-5">
+                <div className="row justify-content-center">
+                    <div className="col-lg-6">
+                        {showChangeCredentials && (
+                            <Card>
+                                <Card.Body>
+                                    <h2 className="text-center mb-4">Dane użytkownika</h2>
+                                    <Form onSubmit={handleChangeCredentials}>
+                                        <Form.Group controlId="formEmail">
+                                            <Form.Label>Adres e-mail</Form.Label>
+                                            <Form.Control type="email" placeholder="example@example.com" value={email}
+                                                          onChange={(e) => setEmail(e.target.value)}/>
+                                        </Form.Group>
+
+                                        <Form.Group controlId="formPassword">
+                                            <Form.Label>Hasło</Form.Label>
+                                            <Form.Control type="password" placeholder="********" value={password}
+                                                          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required
+                                                          onChange={(e) => setPassword(e.target.value)}/>
+                                        </Form.Group>
+
+                                        <Button variant="primary" type="submit" className="w-100 mt-3 mb-3">
+                                            Zatwierdź zmiany
+                                        </Button>
+
+                                        <Button variant="link" onClick={goToAccountDetails}>
+                                            Dane użytkownika
+                                        </Button>
+
+                                        <Button variant="link" onClick={goToAddressDetails}>
+                                            Dane adresowe
+                                        </Button>
+                                    </Form>
+                                </Card.Body>
+                            </Card>
+                        )}
+
+                        {showAccountDetails && (
+                            <Card>
+                                <Card.Body>
+                                    <h2 className="text-center mb-4">Dane użytkownika</h2>
+                                    <Form onSubmit={handleAccountDetails}>
+                                        <Form.Group controlId="formName">
+                                            <Form.Label>Imie</Form.Label>
+                                            <Form.Control type="text" placeholder="Jan" value={name}
+                                                          onChange={(e) => setName(e.target.value)}/>
+                                        </Form.Group>
+
+                                        <Form.Group controlId="formSecondName">
+                                            <Form.Label>Nazwisko</Form.Label>
+                                            <Form.Control type="text" placeholder="Kowalski" value={secondName}
+                                                          onChange={(e) => setSecondName(e.target.value)}/>
+                                        </Form.Group>
+
+                                        <Form.Group controlId="formBirthDate">
+                                            <Form.Label>Data urodzenia</Form.Label>
+                                            <Form.Control type="date" value={birthDate}
+                                                          onChange={(e) => setBirthDate(e.target.value)}/>
+                                        </Form.Group>
+
+                                        <Form.Group controlId="formPhoneNumber">
+                                            <Form.Label>Numer telefonu</Form.Label>
+                                            <Form.Control type="tel" value={phoneNumber} placeholder={"+48 123 456 789"}
+                                                          onChange={(e) => setPhoneNumber(e.target.value)}/>
+                                        </Form.Group>
+
+                                        <Button variant="primary" type="submit" className="w-100 mt-3">
+                                            Zatwierdź zmiany
+                                        </Button>
+
+                                        <Button variant="link" onClick={goToAddressDetails}>
+                                            Dane adresowe
+                                        </Button>
+
+                                        <Button variant="link" onClick={goToChangeCredentials}>
+                                            Zmiana danych logowania
+                                        </Button>
+                                    </Form>
+                                </Card.Body>
+                            </Card>
+                        )}
+
+                        {showAddressDetails && (
+                            <Card>
+                                <Card.Body>
+                                    <h2 className="text-center mb-4">Dane adresowe</h2>
+                                    <Form onSubmit={handleAddressDetails}>
+                                        <Form.Group controlId="formCountry">
+                                            <Form.Label>Kraj</Form.Label>
+                                            <Form.Control type="text" placeholder="Polska" value={country}
+                                                            onChange={(e) => setCountry(e.target.value)}/>
+                                        </Form.Group>
+                                        <Form.Group controlId="formCity">
+                                            <Form.Label>Miasto</Form.Label>
+                                            <Form.Control type="text" placeholder="Warszawa" value={city}
+                                                          onChange={(e) => setCity(e.target.value)}/>
+                                        </Form.Group>
+                                        <Form.Group controlId={"formStreet"}>
+                                            <Form.Label>Ulica</Form.Label>
+                                            <Form.Control type="text" placeholder="ul. Przykładowa" value={street}
+                                                            onChange={(e) => setStreet(e.target.value)}/>
+                                        </Form.Group>
+                                        <Form.Group controlId={"formHouseNumber"}>
+                                            <Form.Label>Numer domu</Form.Label>
+                                            <Form.Control type="text" placeholder="1" value={houseNumber}
+                                                            onChange={(e) => setHouseNumber(e.target.value)}/>
+                                        </Form.Group>
+                                        <Form.Group controlId={"formApartmentNumber"}>
+                                            <Form.Label>Numer mieszkania</Form.Label>
+                                            <Form.Control type="text" placeholder="1" value={apartmentNumber}
+                                                            onChange={(e) => setApartmentNumber(e.target.value)}/>
+                                        </Form.Group>
+                                        <Form.Group controlId={"formPostalCode"}>
+                                            <Form.Label>Kod pocztowy</Form.Label>
+                                            <Form.Control type="text" placeholder="00-000" value={postalCode}
+                                                            onChange={(e) => setPostalCode(e.target.value)}/>
+                                        </Form.Group>
+                                        <Button variant="primary" type="submit" className="w-100 mt-3">
+                                            Zatwierdź zmiany
+                                        </Button>
+                                        <Button variant="link" onClick={goToAccountDetails}>
+                                            Dane użytkownika
+                                        </Button>
+                                        <Button variant="link" onClick={goToChangeCredentials}>
+                                            Zmiana danych logowania
+                                        </Button>
+                                    </Form>
+                                </Card.Body>
+                            </Card>
+                        )}
                     </div>
                 </div>
-                
-                
-                
-      </div>
+            </div>
+            <ToastContainer/>
+        </>
     );
   }
   
